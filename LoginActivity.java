db6fe142b161497b08a6e60c4931b20ccd4a3ee3
Jerry <mrjosorio@gmail.com>
2017-06-18 21:46:34 -0400

    Initial addition of main class

--- LoginActivity.java
+++ LoginActivity.java
@@ -1,14 +1,33 @@
 package com.example.jerry.loginapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText username;
+    Button login;
+
+    Intent i;
+
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        username = (EditText) findViewById(R.id.input_username);
+        login = (Button) findViewById(R.id.login_button);
+    }
+
+    public void getUser(View view){
+        i = new Intent(LoginActivity.this, User.class);
+        i.putExtra("data", username.getText().toString());
+        startActivity(i);
     }
 }
-/Users/jerry/AndroidStudioProjects/LoginApplication/app/src/main/res/mipmap-xhdpi
